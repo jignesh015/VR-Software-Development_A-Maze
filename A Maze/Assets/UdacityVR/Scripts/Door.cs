eec19176e119675ab0dc606190b3f6c65eb64901
Jignesh Patel <patel.jignesh705@gmail.com>
2018-08-19 20:29:27 +0530

    Added Door Behaviour

--- A Maze/Assets/UdacityVR/Scripts/Door.cs
+++ A Maze/Assets/UdacityVR/Scripts/Door.cs
@@ -5,36 +5,52 @@ using UnityEngine;
 public class Door : MonoBehaviour {
 
 	// TODO: Create variables to reference the game objects we need access to
-	// Declare a GameObject named 'leftDoor' and assign the 'Left_Door' game object to the field in Unity
-	// Declare a GameObject named 'rightDoor' and assign the 'Right_Door' game object to the field in Unity
+	public GameObject leftDoor;
+	public GameObject rightDoor;
+
+	public AudioClip doorLockedClip;
+	public AudioClip doorOpeningClip;
 
 	// TODO: Create variables to reference the components we need access to
 	// Declare an AudioSource named 'audioSource' and get a reference to the audio source in Start()
+	public AudioSource audioSource;
+
 
 	// TODO: Create variables to track the gameplay states
-	// Declare a boolean named 'locked' to track if the door has been unlocked and initialize it to 'true'
-	// Declare a boolean named 'opening' to track if the door is opening and initialize it to 'false'
+	private bool locked = true;
+	private bool opening = false;
 
 	// TODO: Create variables to hold rotations used when animating the door opening
 	// Declare a Quaternion named 'leftDoorStartRotation' to hold the start rotation of the 'Left_Door' game object
+	private Quaternion leftDoorStartRotation;
 	// Declare a Quaternion named "leftDoorEndRotation" to hold the end rotation of the 'Left_Door' game object
+	private Quaternion leftDoorEndRotation;
 	// Declare a Quaternion named 'rightDoorStartRotation' to hold the start rotation of the 'Right_Door' game object
+	private Quaternion rightDoorStartRotation;
 	// Declare a Quaternion named 'rightDoorEndRotation' to hold the end rotation of the 'Right_Door' game object
+	private Quaternion rightDoorEndRotation;
 
 	// TODO: Create variables to control the speed of the interpolation when animating the door opening
 	// Declare a float named 'timer' to track the Quaternion.Slerp() interpolation and initialize it to for example '0f'
+	private float timer = 0f;
 	// Declare a float named 'rotationTime' to set the Quaternion.Slerp() interpolation speed and initialize it to for example '10f'
+	private float rotationTime = 10f;
 
 
 	void Start () {
 		// TODO: Get a reference to the audio source
 		// Use GetComponent<>() to get a reference to the AudioSource component and assign it to the 'audioSource'
+		//audioSource = GetComponent<AudioSource>();
 
 		// TODO: Set start and end rotation values used when animating the door opening
 		// Use 'leftDoor' to get the start rotation of the 'Left_Door' game object and assign it to 'leftDoorStartRotation'
+		leftDoorStartRotation = leftDoor.transform.rotation;
 		// Use 'leftDoorStartRotation' and Quaternion.Euler() to set the end rotation of the 'Left_Door' game object and assign it to 'leftDoorEndRotation'
+		leftDoorEndRotation = leftDoorStartRotation * Quaternion.Euler(0,0,90);
 		// Use 'rightDoor' to get the start rotation of the 'Right_Door' game object and assign it to 'rightDoorStartRotation'
+		rightDoorStartRotation = rightDoor.transform.rotation;
 		// Use 'rightDoorStartRotation' and Quaternion.Euler() to set the end rotation of the 'Right_Door' game object and assign it to 'rightDoorEndRotation'
+		rightDoorEndRotation = rightDoorStartRotation * Quaternion.Euler(0,0,-90);
 	}
 
 
@@ -44,6 +60,11 @@ public class Door : MonoBehaviour {
 		// ... use Quaternion.Slerp() to interpolate from 'leftDoorStartRotation' to 'leftDoorEndRotation' by the interpolation time 'timer / rotationTime' and assign it to the 'leftDoor' rotation
 		// ... use Quaternion.Slerp() to interpolate from 'rightDoorStartRotation' to 'rightDoorEndRotation' by the interpolation time 'timer / rotationTime' and assign it to the 'leftDoor' rotation
 		// ... use Time.deltaTime to increment 'timer'
+		if (opening) {
+			leftDoor.transform.rotation = Quaternion.Slerp (leftDoorStartRotation, leftDoorEndRotation, timer);
+			rightDoor.transform.rotation = Quaternion.Slerp (rightDoorStartRotation, rightDoorEndRotation, timer);
+			timer = timer + Time.deltaTime;
+		}
 	}
 
 
@@ -59,12 +80,23 @@ public class Door : MonoBehaviour {
 		// Use 'locked' to check if the door is locked and ...
 		// ... start the animation defined in Update() by changing the value of 'opening'
 		// ... use 'audioSource' to play the AudioClip assigned to the AudioSource component
+		if (!locked) {
+			opening = true;
+			audioSource.clip = doorOpeningClip;
+			audioSource.Play ();
+		} else {
+			audioSource.clip = doorLockedClip;
+			audioSource.Play ();
+		}
 
 		// OPTIONAL-CHALLENGE: Prevent the door from being interacted with after it has started opening
 		// TIP: You could disable the Event Trigger component, or for an extra challenge, try disabling all the Collider components on all children
 
 		// OPTIONAL-CHALLENGE: Play a different sound if the door is locked
 		// TIP: You could get a reference to the 'Door_Locked' audio and play it without assigning it to the AudioSource component
+
+
+		//Debug.Log (audioSource.clip);
 	}
 
 
@@ -77,5 +109,6 @@ public class Door : MonoBehaviour {
 
 		// TODO: Unlock the door 
 		// Unlock the door by changing the value of 'locked'
+		locked = false;
 	}
 }
